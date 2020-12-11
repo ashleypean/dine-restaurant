ffd94d3421b1d581ee5e70bfdd804f12dd30a678
Ashley Pean <pean.ashley@gmail.com>
2020-12-11 14:37:38 -0500

    Basic routing structure for application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './components/Homepage/Homepage'
+import ReservationPage from './components/SetReservation/Reservation'
+import styled from 'styled-components'
+
+const MainApp = styled.div`
+  display: flex;
+  width: 100%; 
+  height: 100%;
+`
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <MainApp>
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage}/>
+          <Route path="/reserve" component={ReservationPage}/>
+          <Route />
+        </Switch>
+      </Router>
+    </MainApp>
   );
 }
 

d64e577db4968807b140e63857d2c22d19ca8db3
Ashley Pean <pean.ashley@gmail.com>
2020-12-14 02:37:32 -0500

    Add styling for desktop and tablet screens

--- src/components/SetReservation/Header/Header.jsx
+++ src/components/SetReservation/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import BackgroundImage from '../../../images/booking/hero-bg-mobile.jpg'
+import BgImageMobile from '../../../images/booking/hero-bg-mobile.jpg'
+import BgImageMobile2x from '../../../images/booking/hero-bg-mobile@2x.jpg'
+import BgImageTablet from '../../../images/booking/hero-bg-tablet.jpg'
+import BgImageTablet2x from '../../../images/booking/hero-bg-tablet@2x.jpg'
+import BgImageDesktop from '../../../images/booking/hero-bg-desktop.jpg'
+import BgImageDesktop2x from '../../../images/booking/hero-bg-desktop@2x.jpg'
 
 const MainDiv = styled.div`
-  background: center / cover no-repeat url(${BackgroundImage});
+  background: center / cover no-repeat url(${BgImageMobile});
   color: white;
   display: flex;
   justify-content: center;
@@ -12,6 +17,22 @@ const MainDiv = styled.div`
   text-align: center;
   padding: 3rem 1rem 8rem;
   position: relative;
+
+  @media screen and (min-width: 400px) {
+    background: center / cover no-repeat url(${BgImageMobile2x});
+  }
+  @media screen and (min-width: 600px) {
+    background: center / cover no-repeat url(${BgImageTablet});
+  }
+  @media screen and (min-width: 800px) {
+    background: center / cover no-repeat url(${BgImageTablet2x});
+  }
+  @media screen and (min-width: 1000px) {
+    background: center / cover no-repeat url(${BgImageDesktop});
+  } 
+  @media screen and (min-width: 1200px) {
+    background: center / cover no-repeat url(${BgImageDesktop2x});
+  }
 `
 
 const Title = styled.h1``
@@ -26,6 +47,7 @@ const Description = styled.p`
   line-height: 1.2rem;
   font-weight: 200;
   margin: 1.2rem 0;
+  max-width: 300px;
 `
 
 const BookingButton = styled.button`
@@ -33,6 +55,7 @@ const BookingButton = styled.button`
   color: white;
   border: .5px solid white;
   width: calc(65% + 2rem);
+  max-width: 250px;
   padding: 1.4rem 1rem;
   letter-spacing: .2rem;
   outline: none;
